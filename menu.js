ec6e5af1f7531c18b849c3d0adcd37bbd509c12b
Dylan Hicks <dylanh333@outlook.com>
2016-12-07 23:22:25 +0800

    Menus can now be re-shown as during their hide animation, and vice vera, making them much more responsive. This is done via a map of menus to running timeouts, which can be looked up and cancelled if another animation starts while the other is running.

--- menu.js
+++ menu.js
@@ -1,6 +1,33 @@
+/* Example Menu System:
+<ul class="Menu -horizontal">
+    <!-- Methods: showMenu, hideMenu, hideChildMenus -->
+
+    <li>
+        <!-- Methods: activateMenuItem -->
+        <a href="#">Menu item</a>
+    </li>
+
+    <li>
+        <!-- Methods: activateMenuItem -->
+        <a href="#">Another menu item</a>
+    </li>
+
+    <li class="-hasSubmenu">
+        <!-- Methods: activateMenuItem -->
+
+        <a href="#">Menu with submenu</a>
+        <ul>
+            <!-- Methods: showMenu, hideMenu, hideChildMenus -->
+            ...
+        </ul>
+    </li>
+</ul>
+*/
+
+
 (function(){
     var stack = [];
-    var ready = true;
+    var timeouts = [];
 
     function $(selector, context){
         if(
@@ -17,10 +44,24 @@
     }
 
     /**
+     * Gets the numerical key for an element, if it has one, otherwise it
+     * assigns a new key and returns that.
+     */
+    var currElementKey = 1; //MUST start from 1
+    function getElementKey(element){
+        var key = Number(element.getAttribute("data-element-key"));
+        if(!key){
+            key = currElementKey++;
+            element.setAttribute("data-element-key", key);
+        }
+        return key;
+    }
+
+    /**
      * showMenu - shows the menu specified by ul, and pushes it to the stack
      */
     function showMenu(ul){
-        if(!ul || ul.classList.contains("-visible")) return;
+        var key = getElementKey(ul);
 
         stack.push(ul);
 
@@ -29,7 +70,9 @@
 
         ul.classList.add("-animating");
         ul.classList.add("-visible");
-        setTimeout(function(){
+        clearTimeout(timeouts[key]);
+        timeouts[key] = setTimeout(function(){
+            delete timeouts[key];
             ul.classList.remove("-animating")
         }, 25);
     }
@@ -41,15 +84,19 @@
      */
     function hideMenu(){
         var ul = null;
+        var key = NaN;
 
         if(stack.length == 0) return;
         else ul = stack.pop();
+        key = getElementKey(ul);
 
         if(!ul.classList.contains("Menu"))
             ul.parentElement.classList.remove("-active");
 
         ul.classList.add("-animating");
-        setTimeout(function(){
+        clearTimeout(timeouts[key]);
+        timeouts[key] = setTimeout(function(){
+            delete timeouts[key];
             ul.classList.remove("-visible");
             ul.classList.remove("-animating");
         }, 1000);
@@ -61,14 +108,11 @@
      * menus if ul is null;
      */
     function hideChildMenus(ul){
-        if(!ready) return;
-        ready = false;
         var backup = stack.slice(0);
         while(stack.length > 0 && stack[stack.length-1] != ul){
             if(stack.length == 1) debugger;
             hideMenu();
         }
-        ready = true;
     }
 
     function hideAllMenus(){
@@ -100,7 +144,7 @@
             });
         });
 
-        //document.addEventListener("click", hideAll);
+        //document.addEventListener("click", hideAllMenus);
         window.addEventListener("blur", hideAllMenus);
     });
 })();
